59d1a8b287c2335d87db61181618faad735daa7c
pavel asadchy <asadchy.pavel@gmail.com>
2022-12-02 21:57:28 +0100

    feat: fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'node:fs/promises';
+import { doesFileExist, getDirName } from './utils/index.js';
+
 const create = async () => {
     // Write your code here 
+    const filePath = 'files/fresh.txt';
+    const content = 'I am fresh and young';
+
+    const path = `${getDirName(import.meta.url)}/${filePath}`;
+    
+    if (await doesFileExist(path)) {
+        throw new Error('FS operation failed');
+    } else {
+        writeFile(path, content);
+    }
 };
 
 await create();
---
+++ src/fs/utils/index.js
@@ -0,0 +1,18 @@
+import { access, constants } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export const doesFileExist = async path => {
+  try {
+    await access(path, constants.R_OK | constants.W_OK);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const getDirName = path => {
+  const __filename = fileURLToPath(path);
+
+  return dirname(__filename);
+};
