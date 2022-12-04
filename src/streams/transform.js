9b6137bc1247aba928f645a9ee2955ec430d2e0a
pavel asadchy <asadchy.pavel@gmail.com>
2022-12-04 19:13:40 +0100

    feat: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { stdout, stdin } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const transformFiles = new Transform({
+        transform(chunk, _enc, cb) {
+            const chunkStringified = chunk.toString();
+            const reverseChunk = chunkStringified
+                .split('')
+                .reverse()
+                .join('');
+            this.push(reverseChunk);
+            cb();
+        }
+    });
+
+    stdin.pipe(transformFiles).pipe(stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    const path = './src/streams/files/fileToWrite.txt';
+    const stream = createWriteStream(path);
+    stdin.pipe(stream);
 };
 
 await write();
